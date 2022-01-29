27f74da05df04474c65fa09d4639676d2bd105ad
root <tem12qaz@yandex.ru>
2022-01-29 20:09:40 +0000

    fix chrome

--- server.py
+++ server.py
@@ -32,6 +32,9 @@ def process():
 if __name__ == '__main__':
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-blink-features")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no sandbox")
+    options.add_argument("--headless")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
