73c110a89c92b5f56bccd7093301b5aa293efbc5
Matvey <tem12qaz@yandex.ru>
2022-01-29 22:44:49 +0300

    n

--- server.py
+++ server.py
@@ -30,7 +30,15 @@ def process():
 
 
 if __name__ == '__main__':
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-blink-features")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("start-maximized")
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", prefs)
     url = 'https://fssp.gov.ru/iss/iP'
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
     app.run()
