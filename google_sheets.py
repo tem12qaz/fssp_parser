6a6cd0d99624077db212081a8dc2abdc1bf39e4a
Matvey <tem12qaz@yandex.ru>
2022-01-30 04:23:13 +0300

    n

--- google_sheets.py
+++ google_sheets.py
@@ -9,14 +9,14 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
                                                                ['https://www.googleapis.com/auth/spreadsheets',
                                                                 'https://www.googleapis.com/auth/drive'])
 
-TABLE_ID = '1AJAH1xoMbFQpdhInkIXbqHcQt3KyUPHICtT-b0xXWtM'
+TABLE_ID = '1No37zLjP4TuxsgcA0_JIj-_7lpIRAdjb-bBOQ-nG1to'
 
 
 def add_to_table(data):
     httpAuth = credentials.authorize(httplib2.Http())  # Авторизуемся в системе
     service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)  # Выбираем работу с таблицами и 4 версию API
 
-    rangeAll = '{0}!A2:Z1000'.format('list_2')
+    rangeAll = '{0}!A2:Z1000'.format('Парсер')
     body = {}
     resultClear = service.spreadsheets().values().clear(spreadsheetId=TABLE_ID, range=rangeAll,
                                                         body=body).execute()
@@ -24,7 +24,7 @@ def add_to_table(data):
     service.spreadsheets().values().batchUpdate(spreadsheetId=TABLE_ID, body={
         "valueInputOption": "USER_ENTERED",  # Данные воспринимаются, как вводимые пользователем (считается значение формул)
         "data": [
-            {"range": "list_2!A2:Z1000",
+            {"range": "Парсер!A2:Z1000",
              "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
              "values": data}
         ]
